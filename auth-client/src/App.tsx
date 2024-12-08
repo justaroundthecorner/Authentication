06849a76a940341f57ff26c801e952835ffad087
israShafique <bcsf16a031@pucit.edu.pk>
2024-12-09 01:44:22 +0300

    enchanced the design and added checks

--- auth-client/src/App.tsx
+++ auth-client/src/App.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Signup from './components/Signup';
 import Signin from './components/Signin';
+import LandingPage from './components/LandingPage';
 import Dashboard from './components/Dashboard';
 
 const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Check if user is authenticated based on localStorage
+    const storedAuth = localStorage.getItem('isAuthenticated');
+    console.log("🚀 ~ useEffect ~ storedAuth:", storedAuth)
+    if (storedAuth === 'true') {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
+      <Route path="/" element={<LandingPage />} />  {/* Default route for LandingPage */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        {/* Protect dashboard route by checking isAuthenticated */}
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? <Dashboard /> : <Navigate to="/signin" />}
+        />
       </Routes>
     </Router>
   );
